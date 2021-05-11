372c55bab9da3495a7fdc51fbed3fc9888786e8b
bayjoh <pblead98@yahoo.com>
2021-05-11 12:38:26 +0100

    Created Routes

--- server.js
+++ server.js
@@ -34,8 +34,81 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 
-//create a basic express route
+//GET request to / to check server health
 app.get("/", (req, res) => res.json({message: "Welcome to Node Class"}));
+
+//POST request to /users to create a new user
+app.post("/users", (req, res) => {
+
+    User.create({
+        name : req.body.name,
+        email: req.body.email,
+        country: req.body.country
+    },(err, newUser) => {
+        if(err)
+            return res.status(500).json({ message: err.message });
+        else
+            return res.status(200).json({ message: "Profile created successfully", data: newUser });
+    });
+
+});
+
+//GET request to /users to fetch all users
+app.get("/users", (req, res) => {
+    User.find({},(err, users) => {
+        if(err)
+            return res.status(500).json({ message: err.message });
+        else
+            return res.status(200).json({ message: "Profiles retrieved successfully", data: users });
+    });
+});
+
+//GET request to /users:id to fetch user by id
+app.get("/users/:id", (req, res) => {
+    User.findById(req.params.id ,(err, user) => {
+        if(err)
+            return res.status(500).json({ message: err.message });
+        else if(!user)
+            return res.status(404).json({ message: "Profile not found", data: user });      
+        else
+            return res.status(200).json({ message: "Profile retrieved successfully", data: user });
+    });
+});
+
+//PUT request to /users:id to update user
+app.put("/users/:id", (req, res) => {
+    User.findByIdAndUpdate(req.params.id,{
+        name : req.body.name,
+        email: req.body.email,
+        country: req.body.country
+    },(err, user) => {
+        if(err)
+            return res.status(500).json({ message: err.message });
+        else if(!user)
+            return res.status(404).json({ message: "Profile not found", data: user });      
+        else{
+            user.save((err, updatedUser) => {
+                if(err)
+                    return res.status(400).json({ message: err.message });
+                else
+                    return res.status(200).json({ message: "Profile updated successfully", data: updatedUser });
+            });
+        }
+            
+    });
+});
+
+//DELETE request to /users:id to delete user by id
+app.delete("/users/:id", (req, res) => {
+    User.findByIdAndDelete(req.params.id ,(err, user) => {
+        if(err)
+            return res.status(500).json({ message: err.message });
+        else if(!user)
+            return res.status(404).json({ message: "Profile not found", data: user });      
+        else
+            return res.status(200).json({ message: "Profile deleted successfully", data: user });
+    });
+});
 
 //Set Port
 const port = process.env.PORT || PORT;
